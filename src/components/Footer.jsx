c6b0ed4a36e24c5b8cbc1780b647a7a71c3eb883
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-01 02:30:48 +0700

    feat: update content footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,23 @@
 function Footer() {
   return (
-    <div className="bg-[#263640] md:px-40 px-10 pt-20 pb-5 mt-20">
-      <div className="flex flex-col md:flex-row md:justify-between gap-10 items-center ">
+    <div className="bg-[#263640] text-gray-300 mx-auto px-6 pt-16 pb-8 mt-20">
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-10">
         <div>
           <img src="/public/icon/Weiss Logo-white.svg" alt="" />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 md:w-sm w-full gap-6 text-white">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-md text-center sm:text-left">
           <div>
-            <div className="font-bold mb-3">Geschäftsführer</div>
-            <div>Beratender Ingenieur Dipl.-Ing. Klaus Weiss</div>
+            <div className="font-bold text-white mb-3">Lead Architect</div>
+            <div>Itsna Maualana (M.Arch)</div>
           </div>
           <div>
-            <div className="font-bold mb-3">Adresse</div>
-            <div>Straßenname 7 12345 Stadt</div>
+            <div className="font-bold text-white mb-3">Adresse</div>
+            <div>State Palace of the Republic of Indonesia</div>
           </div>
         </div>
       </div>
       <div className="h-[1px] w-full bg-gray-500 mt-20"></div>
-      <div className="text-slate-400 mt-5 mb-5">Itsna Maulana | Koda 2025</div>
+      <div className="text-slate-400 mt-5 mb-5">© 2025 WEISS. All Rights Reserved.</div>
     </div>
   );
 }
